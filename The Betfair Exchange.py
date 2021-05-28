9ea0aec20de9534eac8f756799240015676bd28d
devsalmon <devsalmon@users.noreply.github.com>
2021-05-28 14:20:03 +0100

    Market ID can be found

--- The Betfair Exchange.py
+++ The Betfair Exchange.py
@@ -6,14 +6,19 @@ endpoint = "https://api.betfair.com/exchange/betting/rest/v1.0/"
 APP_KEY = 'iym7OLs6r9EYcFYv'
 SESSION_TOKEN = 'iCauS/9fh/fSS32gro11CbPcbd8e9RAYFedsMU9e8F4='
 footballID = "1"
-START_DATE = "2021-05-27T00:00:00Z"
-END_DATE = "2021-05-27T23:59:00Z"
+eventID = ''
+marketID = ''
+START_DATE = "2021-05-29T18:59:00Z"
+END_DATE = "2021-05-29T19:59:00Z"
 football_events_url = endpoint + "listEvents/"
 football_match_url = endpoint + "listMarketCatalogue/"
 match_market_url = endpoint + "listMarketBook/"
 
+match_sought_for = input("Football Match: ")
+
 header = { 'X-Application' : APP_KEY, 'X-Authentication' : SESSION_TOKEN,'content-type' : 'application/json' }
- 
+
+## Requests list of football events between selected dates.
 ## eventTypeIds is the id of event type.
 json_req_football_events='{"filter":{ "eventTypeIds": ["'+footballID+'"], "marketStartTime": {"from": "'+START_DATE+'", "to": "'+END_DATE+'"} }}'
 
@@ -37,8 +42,22 @@ json_req_football_events='{"filter":{ "eventTypeIds": ["'+footballID+'"], "marke
 ##    }
 ##]
 
+## When printed, returns list of football matches between selected dates. 
+football_events_response = requests.post(football_events_url, data=json_req_football_events, headers=header)
+## Converts response to json.
+formatted_football_events = json.loads(football_events_response.text)
+##number_of_events = len(formatted_football_events)
+## for each event...
+for event in formatted_football_events:
+    ## if the event name is equal to sought for event...
+    if event['event']['name'] == match_sought_for:
+        print("Event Found!")
+        ## get the ID of that event.
+        eventID = event['event']['id']
+        print("Event ID: " + eventID)
+
+## Requests market information for selected match.   
 ## eventIDs is the id of the match
-eventID = "30494247"
 json_req_football_match='{"filter":{ "eventIds": ["'+eventID+'"] }, "maxResults": "200", "marketProjection": ["COMPETITION", "EVENT", "EVENT_TYPE", "RUNNER_DESCRIPTION", "RUNNER_DESCRIPTION", "RUNNER_METADATA", "MARKET_START_TIME"]}'
 
 ##                  LIST EVENT INFO
@@ -65,7 +84,21 @@ json_req_football_match='{"filter":{ "eventIds": ["'+eventID+'"] }, "maxResults"
 ##        "id": 1
 ##    }
 ##]
-marketID = "1.183050605"
+
+## When printed, returns list of markets for selected match.
+football_match_markets_response = requests.post(football_match_url, data=json_req_football_match, headers=header)
+## Converts response to json.
+formatted_match_markets = json.loads(football_match_markets_response.text)
+## for each market...
+for market in formatted_match_markets:
+    ## if the market name is equal to sought for market...
+    if market['marketName'] == "Match Odds":
+        print("Market Found!")
+        ## get the ID of that market.
+        marketID = market['marketId']
+        print("Market ID: " + marketID)
+
+## Requests odds for selected market of the match.
 ## market id is id of market for that match
 json_req_market_odds='{"marketIds": ["'+marketID+'"], "priceProjection": {"priceData": ["EX_BEST_OFFERS", "EX_TRADED"], "virtualise": "true" }}'
 
@@ -107,17 +140,13 @@ json_req_market_odds='{"marketIds": ["'+marketID+'"], "priceProjection": {"price
 ##    }
 ##]
 
-## When printed, returns list of football matches between selected dates. 
-football_events_response = requests.post(football_events_url, data=json_req_football_events, headers=header)
-## When printed, returns list of markets for selected match.
-football_match_response = requests.post(football_match_url, data=json_req_football_match, headers=header)
 ## When printed, returns odds for selected market.
 match_market_response = requests.post(match_market_url, data=json_req_market_odds, headers=header)
 
 ## Prints list of football events
-print(json.dumps(json.loads(football_events_response.text), indent=3))
+##print(json.dumps(json.loads(football_events_response.text), indent=3))
 ## Prints football match info
-##print(json.dumps(json.loads(football_match_response.text), indent=3))
+print(json.dumps(json.loads(football_match_markets_response.text), indent=3))
 ## Prints odds for market
 ##print(json.dumps(json.loads(match_market_response.text), indent=3))
 ##y = json.loads(match_market_response.text)
